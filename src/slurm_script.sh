fb53b54b9e7febd76079712fd47e59e4758b0353
hugo <metallic-silver@hotmail.com>
2022-08-23 23:17:20 -0300

    slurm

--- src/slurm_script.sh
+++ src/slurm_script.sh
@@ -2,6 +2,6 @@
 
 . $HOME/my_cc_scripts/custom_commands.sh
 rm -rf data/datasets/tiny-imagenet
-cp data/datasets/tiny-imagenet_backup ${SLURM_TMPDIR}/tiny-imagenet
+cp -r data/datasets/tiny-imagenet_backup ${SLURM_TMPDIR}/tiny-imagenet
 ln -s ${SLURM_TMPDIR}/tiny-imagenet data/datasets/tiny-imagenet
 CC_MY_SINGULARITY_EXEC pipenv run python -m framework.main --logfile log.txt --gridsearch_name reproduce --method_name HAT --ds_name tiny --lr_grid 1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4 --boot_lr_grid 1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4 --test base_VGG9_cl_512_512
